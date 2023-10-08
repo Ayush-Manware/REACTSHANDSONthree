033b0ace15607e96881b228575603737327ec2ec
Ayush-Manware <ayushmanware19@gmail.com>
2023-10-08 16:28:09 +0530

    Completed

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+// import Form from './Form';
+import Forms from './component/Forms';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Form /> */}
+      <Forms />
+    </div>
+  );
+}
+
+export default App;
